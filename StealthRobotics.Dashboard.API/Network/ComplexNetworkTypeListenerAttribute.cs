b651368b979c2ea6d6a9b60faa82abe096bdf96f
Sean Dempsey <sean@demps.changeip.net>
2019-01-13 13:04:31 -0800

    Added an attribute to label a control which listens only to specific network table types

---
+++ StealthRobotics.Dashboard.API/Network/ComplexNetworkTypeListenerAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealthRobotics.Dashboard.API.Network
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class ComplexNetworkTypeListener : Attribute
+    {
+        public string[] TableTypes { get; }
+
+        public ComplexNetworkTypeListener(params string[] tableTypes)
+        {
+            TableTypes = tableTypes;
+        }
+    }
+}
