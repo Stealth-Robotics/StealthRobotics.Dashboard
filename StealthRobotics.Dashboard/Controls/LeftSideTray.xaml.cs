67f55f2ed94bda4d68ea3c5686ae170244e5e4c3
Sean Dempsey <sean@demps.changeip.net>
2019-01-14 22:27:19 -0800

    Added the slideout tray. Todo: Display over other adorners

---
+++ StealthRobotics.Dashboard/Controls/LeftSideTray.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Adorners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using StealthRobotics.Dashboard.API.UI;
+
+namespace StealthRobotics.Dashboard.Controls
+{
+    /// <summary>
+    /// Interaction logic for LeftSideTray.xaml
+    /// </summary>
+    public partial class LeftSideTray : AdornedControl
+    {
+        private const double maxSlideInTime = 0.25;
+        private const double maxSlideOutTime = 0.25;
+        private ThicknessAnimation slideOut = null;
+        private Thickness defaultMargin;
+
+        public LeftSideTray()
+        {
+            InitializeComponent();
+        }
+
+        private void Adorner_Click(object sender, RoutedEventArgs e)
+        {
+            this.BringToFront();
+            if(slideOut == null)
+            {
+                //slide out
+                double slideTime = maxSlideOutTime * (-Margin.Left / ActualWidth);
+                slideOut = new ThicknessAnimation(new Thickness(0), new Duration(TimeSpan.FromSeconds(slideTime)));
+                BeginAnimation(MarginProperty, slideOut);
+            }
+            else
+            {
+                //cancel the slide out
+                slideOut = null;
+                //slide out
+                double slideTime = maxSlideInTime * (1 + Margin.Left / ActualWidth);
+                ThicknessAnimation slideIn = new ThicknessAnimation(defaultMargin, new Duration(TimeSpan.FromSeconds(slideTime)));
+                BeginAnimation(MarginProperty, slideIn);
+            }
+        }
+
+        private void AdornedControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Margin = new Thickness(-ActualWidth, 0, 0, 0);
+            defaultMargin = Margin;
+        }
+    }
+}
