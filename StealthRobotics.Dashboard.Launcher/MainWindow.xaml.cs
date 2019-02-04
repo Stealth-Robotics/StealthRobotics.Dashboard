68a6cb8310a16ef952b54da2ba281c41c060e9f8
Sean Dempsey <sean@demps.changeip.net>
2019-02-03 19:31:35 -0800

    Updated task list and started on launcher. May put launcher on MEF to make it fewer lines to start whatever target and to add new targets.

---
+++ StealthRobotics.Dashboard.Launcher/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StealthRobotics.Dashboard.Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Wpf_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Java_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Jfx_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ov_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
