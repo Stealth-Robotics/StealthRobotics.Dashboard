bb6dd4b07fbaaa208333c52afe83946cf9083d01
Sean Dempsey <sean@demps.changeip.net>
2019-02-19 23:22:22 -0800

    Made hololens dashboard an actual option

--- StealthRobotics.Dashboard.Launcher/Parts/HololensDashboard.cs
+++ StealthRobotics.Dashboard.Launcher/Parts/HololensDashboard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,19 @@ namespace StealthRobotics.Dashboard.Launcher.Parts
     {
         public void ClickAction()
         {
-            MessageBox.Show("New tech coming soon from Stealth Robotics.", "Oops! That's not ready!",
-                MessageBoxButton.OK, MessageBoxImage.Information);
+            //MessageBox.Show("New tech coming soon from Stealth Robotics.", "Oops! That's not ready!",
+            //    MessageBoxButton.OK, MessageBoxImage.Information);
+            //todo: check desktop, if not there, let the user know it needs to be.
+            string dashboard = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\HoloDash.exe";
+            if (File.Exists(dashboard))
+            {
+                ProgramStarter.Start($"\"{dashboard}\"");
+            }
+            else
+            {
+                MessageBox.Show("Couldn't find the dashboard. Make sure the dashboard is on the desktop and named HoloDash.exe.", 
+                    "Oops! Couldn't find that.", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
